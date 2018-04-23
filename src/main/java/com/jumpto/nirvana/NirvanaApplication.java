7c5eddfc6ef055d8e65d4c1911a19992f8b4f61e
Simon <sqeebs@gmail.com>
2018-04-23 12:07:54 +0200

    First commit

---
+++ src/main/java/com/jumpto/nirvana/NirvanaApplication.java
@@ -0,0 +1,12 @@
+package com.jumpto.nirvana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NirvanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NirvanaApplication.class, args);
+	}
+}
